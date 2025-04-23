af667e276a36b34c5e8a6bef7ab3af25924ae29e
shashankmj@365smartshop.com <shashankmj@nousinfo.com>
2025-04-23 16:01:56 +0530

    Added Logo component

---
+++ src/components/HeaderLogo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HeaderLogo.css';
+import logo from '../assets/sahil-holidays-logo.png';
+
+const HeaderLogo = () => {
+  return (
+    <header className="header-logo-container">
+      <div className="logo-wrapper">
+        <img src={logo} alt="Sahil Holidays Logo" className="header-logo-img" />
+      </div>
+    </header>
+    
+  );
+};
+
+export default HeaderLogo;
