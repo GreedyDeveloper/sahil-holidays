75f3bc82764395454d969e043e5ae56f30db1a5a
Shashank <shashank.jaya.jain@gmail.com>
2025-06-12 18:41:54 +0530

    packages are fetched from backend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import React, { useEffect, createContext, useState } from 'react';
 import 'App.css';
 import HeaderLogo from 'components/HeaderLogo';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import {muiTheme} from 'styles/theme';
-import Navigator from './Navigator'; 
+import { muiTheme } from 'styles/theme';
+import Navigator from './Navigator';
+import { fetchData } from 'services/Api';
+
+export const DataContext = createContext(null);
 
 function App() {
+
+  const [data, setData] = useState({
+    packages: [],
+    destinations: [],
+    cities: [],
+  });
+
+  useEffect(() => {
+    fetchData().then((packages) => {
+      const destinationNames = new Set();
+      const cities = new Set();
+      packages.forEach(pkg => {
+        if (pkg.destination) {
+          destinationNames.add(pkg.destination.trim());
+        }
+        if (pkg.cities) {
+          pkg.cities.forEach(city => cities.add(city.trim()));
+        }
+      });
+
+      const destinations = Array.from(destinationNames).map(name => {
+        return { name, image: packages.find(pkg => pkg.destination.trim() === name)?.image || '' };
+      })
+
+      setData({
+        packages,
+        destinations: destinations,
+        cities: Array.from(cities),
+      });
+    }).catch((error) => {
+      console.error("Error fetching packages:", error);
+    });
+  }, []);
+
   return (
     <ThemeProvider theme={muiTheme}>
       <CssBaseline />
-      <div className="App">
-        <HeaderLogo />
-        <Navigator />
-      </div>
+      <DataContext.Provider value={data}>
+        <div className="App">
+          <HeaderLogo />
+          <Navigator />
+        </div>
+      </DataContext.Provider>
     </ThemeProvider>
   );
 }
